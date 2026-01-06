87461a48f310c26bbf9e310d5ff9e8e3685945fa
MariuszMalec <mario106c@gmail.com>
2026-01-06 20:57:21 +0100

    add live by person

--- activity_api_new/routes_activities_home.py
+++ activity_api_new/routes_activities_home.py
@@ -1,4 +1,4 @@
-from fastapi import Request, Form
+from fastapi import Request, Form, Query
 from fastapi.responses import RedirectResponse, HTMLResponse
 from starlette.status import HTTP_303_SEE_OTHER
 import sqlite3
@@ -87,6 +87,79 @@ def register_routes_home(app):
         )
 
     
+    @app.get("/homebyperson", response_class=HTMLResponse)
+    def home_page(request: Request, person: str = Query(default="MAMA") ):
+        db = get_db()
+        cursor = db.cursor()
+
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        iso_day = now.isoweekday()
+        current_day = system_day_to_db_day(iso_day)
+        current_day_name = now.strftime("%A")       
+
+        rows = cursor.execute("""
+            SELECT
+                ad.StartTime,
+                ad.EndTime,
+                ad.Description,
+                pf.PersonName,
+                pf.PersonPicture,
+                pa.Picture
+            FROM ActiviesDays ad
+            LEFT JOIN PersonFamilies pf
+                ON ad.ModelPersonFamilyId = pf.Id
+            LEFT JOIN PictureActivities pa
+                ON ad.ModelPictureActivityId = pa.Id
+            WHERE ad.DayOfWeek = ?
+            ORDER BY ad.StartTime
+        """, (current_day,)).fetchall()
+
+        db.close()
+
+        PERSON_NAME_MAP = {
+            1: "TATA",
+            2: "MAMA",
+            3: "GOSIA",
+            4: "EMILKA",
+            5: "ALL",
+        }
+
+        current_items = []
+        next_items = []
+
+        for r in rows:
+            item = {
+                "start": r["StartTime"],
+                "end": r["EndTime"],
+                "description": r["Description"],
+                "person": PERSON_NAME_MAP.get(r["PersonName"], ""),
+                "personPicture": r["PersonPicture"],
+                "picture": r["Picture"],
+            }
+
+            # 🔴 TERAZ
+            if r["StartTime"] <= current_time <= r["EndTime"]:
+                current_items.append(item)
+
+            # 🔵 NASTĘPNIE
+            elif r["StartTime"] > current_time:
+                next_items.append(item)
+
+        return templates.TemplateResponse(
+            "statusbyperson.html",
+            {
+                "request": request,
+                "now": current_time,
+                "current": current_items,
+                "next": next_items,
+                "current_day_name": current_day_name,
+            }
+        )
+
+        
+
+
     @app.get("/activities")
     def root_redirect():
         return RedirectResponse("/activities", status_code=302)       
