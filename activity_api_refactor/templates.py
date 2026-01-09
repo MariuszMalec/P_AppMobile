9490b4d72f31d25f9c4357de8df6a49cbb356cbb
MariuszMalec <mario106c@gmail.com>
2026-01-09 17:53:39 +0100

    fix tests

--- activity_api_refactor/templates.py
+++ activity_api_refactor/templates.py
@@ -1,3 +1,8 @@
+from pathlib import Path
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
+BASE_DIR = Path(__file__).resolve().parent
+
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates"
+)
