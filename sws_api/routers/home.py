c022bcc973efd4d0d4eccbab401bf211b5c1ce08
MariuszMalec <mario106c@gmail.com>
2026-09-07 22:58:48 +0200

    fix home for mobile

--- sws_api/routers/home.py
+++ sws_api/routers/home.py
@@ -1,11 +1,7 @@
-from fastapi import APIRouter, Request, Form, Query, Depends
-from fastapi.responses import RedirectResponse, HTMLResponse
-from starlette.status import HTTP_303_SEE_OTHER
-import sqlite3
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
 from templates import templates
-
 from db import get_db
-from datetime import datetime
 
 
 router = APIRouter(
@@ -14,30 +10,14 @@ router = APIRouter(
 )
 
 
-# ==============================
-# HOME
-# ==============================
 @router.get("", response_class=HTMLResponse)
 def home_page(
     request: Request,
-    db = Depends(get_db)
+    db=Depends(get_db)
 ):
-    
-    try:
-        return templates.TemplateResponse(
-            request,
-            "home.html",
-            status_code=200
-        )
-
-    except Exception:
-        # brak bazy / tabel / inny błąd → pokaż stronę z komunikatem
-        return templates.TemplateResponse(
-            request,
-            "home.html",
-            status_code=400
-        )
-
-    finally:
-        db.close()
-
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request
+        }
+    )
